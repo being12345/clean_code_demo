6f57cce53d3785c30014e2812525bf3f52125880
being <lizhuofeng12345@gmail.com>
2023-05-22 14:21:51 +0800

    init

---
+++ src/main/java/org/example/Primes.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.util.Arrays;
+
+/**
+ * @className: Primes
+ * @description: generate primes(practice for chapter4)
+ * @Author: zhuofengli
+ * @Date: 2023/5/22 13:50
+ */
+public class Primes {
+    public static boolean[] isPrimes;
+
+    public static int[] generatePrimes(int maxValue) {
+        if (maxValue >= 2) {
+            int s = maxValue + 1;
+
+            initisPrimes(maxValue);
+
+            for (int i = 2; i < Math.sqrt(s) + 1; i++) {
+                if (isPrimes[i]) {
+                    for (int j = 2 * i; j < s; j += i) {
+                        isPrimes[j] = false;
+                    }
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < s; i++) {
+                if (isPrimes[i]) {
+                    count++;
+                }
+            }
+            int[] primes = new int[count];
+
+            for (int i = 0, j = 0; i < s; i++) {
+                if (isPrimes[i]) {
+                    primes[j++] = i;
+                }
+            }
+            return primes;
+        } else {
+            return new int[0];
+        }
+    }
+
+    public static void initisPrimes(int maxValue) {
+        isPrimes = new boolean[maxValue + 1];
+
+        isPrimes[0] = isPrimes[1] = false;
+        for (int i = 2; i < maxValue + 1; i++) {
+            isPrimes[i] = true;
+        }
+    }
+
+    public static void main(String[] args) {
+        String expected = "[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]";
+        String actual = Arrays.toString(generatePrimes(50));
+        if (expected.equals(actual)) {
+            System.out.println(true);
+        }
+
+    }
+}
---
+++ src/test/java/org/example/PrimesTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @className: PrimesTest
+ * @description: TODO
+ * @Author: zhuofengli
+ * @Date: 2023/5/22 13:52
+ */
+class PrimesTest {
+    @org.junit.jupiter.api.Test
+    void generatePrimes() {
+
+    }
+}
